bdd928fc25ea53af557950fdd743e980579ce385
Rama Kocherlakota <ramakocherlakota@gmail.com>
2022-05-13 17:57:15 -0400

    split out the entropy sum query

--- wordler/QuordlePal.py
+++ wordler/QuordlePal.py
@@ -10,25 +10,31 @@ class QuordlePal(WordlePal):
         
         tables = []
         for i in range(len(responses)) :
-            tables.append(f"q_{i}")
             self.guess_table(f"q_{i}", guesses, responses[i])
+            self.collate_table(f"q_{i}", f"r_{i}")
+            tables.append(f"r_{i}")
         return self.min_sum_entropy(tables)
+
+    def collate_table(self, source, dest) :
+        self.query(f"drop {self.temporary} table if exists {dest}")
+        hard_clause = ""
+        if self.hard_mode :
+            hard_clause = " and hard "
+        self.query(f"create {self.temporary} table {dest} as select g.guess, sum({source}.c * log({source}.c) / log(2)) / sum({source}.c) as h from guesses g, {source} where {source}.guess = g.guess {hard_clause} group by 1")
+        self.query(f"alter table {dest} add key(guess)")
 
     def min_sum_entropy(self, tables) :
         sums = []
         wheres = []
         hards = []
         for table in tables:
-            sums.append(f"sum({table}.c * log({table}.c) / log(2)) / sum({table}.c)")
+            sums.append(f"{table}.h")
             wheres.append(f"{table}.guess = g.guess")
             hards.append(f"{table}.hard")
-        hard_mode_clause = ""
-        if self.hard_mode:
-            hard_mode_clause = " and (" + " or ".join(hards)
         sums_clause = " + ".join(sums)
         tables_clause = ", ".join(tables)
-        where_clause = " and ".join(wheres) + hard_mode_clause
-        sql = f"select g.guess, {sums_clause} from guesses g, {tables_clause} where {where_clause} group by 1 order by 2, 1 limit 1"
+        where_clause = " and ".join(wheres)
+        sql = f"select g.guess, {sums_clause} from guesses g, {tables_clause} where {where_clause} order by 2 limit 1"
         csr = self.query(sql, "min sum entropy")
         for (guess, entropy) in csr:
             return (guess, entropy)
