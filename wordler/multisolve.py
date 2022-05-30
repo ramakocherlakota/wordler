4f533e6d09f8b0e3433d8cf64adcc83e71505d4d
Rama Kocherlakota <ramakocherlakota@gmail.com>
2022-05-30 15:08:37 -0400

    added logs of hard and not-hard mode runs

--- wordler/multisolve.py
+++ wordler/multisolve.py
@@ -1,24 +1,11 @@
 import fileinput, sys
 import QuordlePal
 
-hard_mode = False
+hard_mode = True
 keep_temporary = False
 debug = False
 scores_table = "scores"
 starting_word = "raise"
-
-if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv)):
-        arg = sys.argv[i]
-        if arg.startswith("-"):
-            if arg == "--hard":
-                hard_mode = True
-            if arg == "--debug":
-                debug = True
-            if arg == "--keep-temporary":
-                keep_temporary = True
-            if arg == "--all":
-                scores_table = "all_scores"
 
 quordler = QuordlePal.QuordlePal(debug=debug, scores_table=scores_table, starting_word=starting_word, hard_mode=hard_mode, keep_temporary=keep_temporary)
 
